00961da189712090d8cefb92c00916c873a6982f
reducesteps <jonathanoakey0@gmail.com>
2023-08-15 05:13:39 +0000

    Made sections collapsible

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-// Add any JavaScript functionality here
-
-// Dark theme toggle
-const body = document.querySelector('body');
-const themeToggleBtn = document.createElement('button');
-themeToggleBtn.innerText = 'Toggle Theme';
-themeToggleBtn.style.position = 'fixed';
-themeToggleBtn.style.bottom = '10px';
-themeToggleBtn.style.right = '10px';
-body.appendChild(themeToggleBtn);
-
-themeToggleBtn.addEventListener('click', () => {
-    body.classList.toggle('dark-theme');
-});
-
-// Simple animation for header on page load
-const header = document.querySelector('header');
-header.style.opacity = '0';
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        header.style.transition = 'opacity 1s';
-        header.style.opacity = '1';
-    }, 500);
+document.addEventListener('DOMContentLoaded', (event) => {
+    let coll = document.getElementsByClassName('collapsible');
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+            let content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + 'px';
+            }
+        });
+    }
 });
